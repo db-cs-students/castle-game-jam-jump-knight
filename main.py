92db57cbb775d27dc75dca87e6b1eb7ff743df02
Nicholas <69809504+Scryguy101@users.noreply.github.com>
2020-09-28 10:14:04 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Title:
-Creators:
-Description:
+Title: Jump Knight
+Creators: Nick R. Jackson C.
+Description: Game Jam: Castle, knight themed jump game with combat late game.
 """
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 /** 
-Title:
-Creators:
-Description:
+Title: Jump Knight
+Creators: Nick R. Jackson C.
+Description: Game Jam: Castle, knight themed jump game with combat late game.
 
  */
